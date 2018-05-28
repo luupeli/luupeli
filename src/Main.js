e86c5baa6423f100035a17478473f23c4a057cc4
KeremAtak <kerem.atak@hotmail.com>
2018-05-28 11:39:23 +0300

    minor fixes on writinggame

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import WritingGame from './WritingGame'
 import SelectGameMode from './menus/SelectGameMode'
+import EndScreen from './menus/EndScreen'
 import Home from './menus/Home'
+import GameSettings from './menus/GameSettings'
 
 const Main = () => (
   <main>
@@ -10,6 +12,8 @@ const Main = () => (
       <Route exact path='/' component={Home} />
       <Route exact path='/game' component={SelectGameMode} />
       <Route exact path='/writinggame' component={WritingGame} />
+      <Route exact path='/settings' component={GameSettings} />
+      <Route exact path='/endscreen' component={EndScreen} />
     </Switch>
   </main>
 )
