189ced117eab1f76d2eb9004fa17dd5891b81c06
Nabscina <bebenoctescere@gmail.com>
2018-05-28 11:10:01 +0300

    page for game settings

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import WritingGame from './WritingGame'
 import SelectGameMode from './menus/SelectGameMode'
 import Home from './menus/Home'
+import GameSettings from './menus/GameSettings'
 
 const Main = () => (
   <main>
@@ -10,6 +11,7 @@ const Main = () => (
       <Route exact path='/' component={Home} />
       <Route exact path='/game' component={SelectGameMode} />
       <Route exact path='/writinggame' component={WritingGame} />
+      <Route exact path='/settings' component={GameSettings} />
     </Switch>
   </main>
 )
--- src/menus/SelectGameMode.js
+++ src/menus/SelectGameMode.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const SelectGameMode = () => (
-	<div className="App">
+	<div>
+		<div className="App">
 		<h1 className="h1">Valitse peli:</h1>
 		<div className="btn-group">
-			<Link to='/writinggame'><button>Kirjoituspeli</button></Link>
+			<Link to='/settings'><button>Kirjoituspeli</button></Link>
 			<button>...</button>
 			<button>...</button>
 		</div>
-		<div>
-			<Link to='/'><button className="gobackbutton">Takaisin</button></Link>
+		</div>
+		<div className="btn-group">
+			<Link to='/'><button>Takaisin</button></Link>
 		</div>
 	</div>
 )
