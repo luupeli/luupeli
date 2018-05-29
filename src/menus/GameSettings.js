8806b8cbe3307b042d8301304454b34581ea784c
Pebe <Nabscina@users.noreply.github.com>
2018-05-29 14:28:48 +0300

    settings page: first option is checked by default

--- src/menus/GameSettings.js
+++ src/menus/GameSettings.js
@@ -5,6 +5,7 @@ class GameSettings extends React.Component {
 
 	constructor(props) {
 		super(props);
+
 	}
 
 	render() {
@@ -13,27 +14,27 @@ class GameSettings extends React.Component {
 				<div className="App settingspage">
 					<h1 className="h2">Valitse eläin:</h1>
 					<form>
-						<label className="radio-inline"><input type="radio" name="name"></input>Koira</label>
-						<label className="radio-inline"><input type="radio" name="name"></input>Kissa</label>
-						<label className="radio-inline"><input type="radio" name="name"></input>Hevonen</label>
-						<label className="radio-inline"><input type="radio" name="name"></input>Gorilla</label>
+						<label className="radio-inline"><input type="radio" name="name" id="1" defaultChecked></input>Koira</label>
+						<label className="radio-inline"><input type="radio" name="name" id="2"></input>Kissa</label>
+						<label className="radio-inline"><input type="radio" name="name" id="3"></input>Hevonen</label>
+						<label className="radio-inline"><input type="radio" name="name" id="4"></input>Gorilla</label>
 					</form>
 					<h1 className="h2">Valitse ruumiinosa:</h1>
 					<form>
-						<label className="radio-inline"><input type="radio"></input>Pää</label>
-						<label className="radio-inline"><input type="radio"></input>Keho</label>
-						<label className="radio-inline"><input type="radio"></input>Eturaaja</label>
-						<label className="radio-inline"><input type="radio"></input>Takaraaja</label>
+						<label className="radio-inline"><input type="radio" id="head" defaultChecked></input>Pää</label>
+						<label className="radio-inline"><input type="radio" id="body"></input>Keho</label>
+						<label className="radio-inline"><input type="radio" id="front"></input>Eturaaja</label>
+						<label className="radio-inline"><input type="radio" id="back"></input>Takaraaja</label>
 					</form>
 					<h1 className="h2">Pelin pituus:</h1>
 					<form>
-						<label className="radio-inline"><input type="radio" name="1"></input>10</label>
+						<label className="radio-inline"><input type="radio" name="1" defaultChecked></input>10</label>
 						<label className="radio-inline"><input type="radio" name="1"></input>20</label>
 						<label className="radio-inline"><input type="radio" name="1"></input>30</label>
 					</form>
 					<h1 className="h2">Vaikeusaste:</h1>
 					<form>
-						<label className="radio-inline"><input type="radio" name="1"></input>Helppo</label>
+						<label className="radio-inline"><input type="radio" name="1" defaultChecked></input>Helppo</label>
 						<label className="radio-inline"><input type="radio" name="1"></input>Keskivaikea</label>
 						<label className="radio-inline"><input type="radio" name="1"></input>Vaikea</label>
 					</form>
