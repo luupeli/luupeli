9462476eb5e4fd20f97f9186a05b0e8dee1ad80f
KeremAtak <kerem.atak@hotmail.com>
2018-07-09 11:05:06 +0300

    travis testing

--- src/teardown.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = async function() {
-    // close the browser instance
-    await global.__BROWSER__.close();
-  
-    // clean-up the wsEndpoint file
-    rimraf.sync(DIR);
-};
-  
