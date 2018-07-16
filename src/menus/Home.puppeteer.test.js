1c6cd920b53d9b01a40dad831dd56ce15bcbfae8
Helena Parviainen <helena.m.parviainen@gmail.com>
2018-07-16 12:23:56 +0300

    waitForSelectors added to more tests to prevent undefined id errors when running tests

--- src/menus/Home.puppeteer.test.js
+++ src/menus/Home.puppeteer.test.js
@@ -76,27 +76,33 @@ describe("Home (puppeteer) tests", () => {
   })
 
   test('page renders', async () => {
+	  await page.waitForSelector('#homeMenu')
     const textContent = await page.$eval('#homeMenu', el => el.textContent)
     expect(textContent.includes("Luupeli")).toBe(true)
   }, 20000)
 
   test('Pelaa button redirects to SelectGameMode page', async () => {
-    
+    await page.waitForSelector('#proceedToSelectGameMode')
     await page.click('#proceedToSelectGameMode')
+    await page.waitForSelector('#gameBody')
     const textContent = await page.$eval('#gameBody', el => el.textContent)
     expect(textContent.includes("Kirjoituspeli")).toBe(true)
   }, 2500)
 
   test('Login button redirects to Login page', async () => {
     
+    await page.waitForSelector('#homeMenuLoginButton')
     await page.click('#homeMenuLoginButton')
+    await page.waitForSelector('#loginPromptMenu')
     const textContent = await page.$eval('#loginPromptMenu', el => el.textContent)
     expect(textContent.includes("Luukirjaudu sisään")).toBe(true)
   }, 2500)
 
   test('Sign-up button redirects to Sign-up page', async () => {
     
+    await page.waitForSelector('#homeMenuSignUpButton')
     await page.click('#homeMenuSignUpButton')
+    await page.waitForSelector('#registerMenu')
     const textContent = await page.$eval('#registerMenu', el => el.textContent)
     expect(textContent.includes("luukäyttäjätunnus")).toBe(true)
   }, 2500)
@@ -108,6 +114,7 @@ describe("Home (puppeteer) tests", () => {
     
 	await page.waitForSelector('#themeChangeButton')
     await page.click('#themeChangeButton')
+    await page.waitForSelector('#styleName')
     const textContent = await page.$eval('#styleName', el => el.textContent)
     expect(textContent.includes("blood-dragon")).toBe(false)
   }, 2500)
