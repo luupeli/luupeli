3705124a0b80b98c388a8fc4f940c9dbaa1dd471
KeremAtak <kerem.atak@hotmail.com>
2018-08-08 16:12:00 +0300

    statistics page shows real stuff now

--- src/menus/admin/Statistics.js
+++ src/menus/admin/Statistics.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Link, Redirect } from 'react-router-dom'
+import gameSessionService from '../../services/gameSessions'
+import gameSessions from '../../services/gameSessions';
 
 class Statistics extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
+			gameSessions: [],
+			gameSessionsFiltered: [],
+			timePlayed: 0.0,
+			writingGameCount: 0,
+			mixedGameCount: 0,
+			multipleChoiceGameCount: 0
 		}
 		window.onunload = function () { window.location.href = '/' }
+		this.setStats = this.setStats.bind(this)
+		this.setTimePlayed = this.setTimePlayed.bind(this)
+		this.setGameTypes = this.setGameModes.bind(this)
+
 	}
 
 	componentDidMount() {
@@ -17,12 +29,50 @@ class Statistics extends React.Component {
 				this.setState({ redirect: true, redirectTo: '/' })
 			}
 			this.setState({ user })
+			gameSessionService.getAll()
+				.then((response) => {
+					this.setStats(response)
+				})
+				.catch((error) => {
+					console.log(error)
+				})
 		} else {
 			this.setState({ redirect: true, redirectTo: '/' })
 		}
 	}
 
+	setStats(response) {
+		this.setState({ gameSessions: response.data })
+		this.setState({ gameSessionsFiltered: response.data })
+		this.setTimePlayed()
+		this.setGameModes()
+	}
+
+	setTimePlayed() {
+		let seconds = 0
+		this.state.gameSessionsFiltered.forEach(function(session){
+			seconds += session.seconds
+		})
+		this.setState({ timePlayed : Math.round(seconds) })
+	}
+
+	setGameModes() {
+		let writingGame = this.state.gameSessionsFiltered.filter(session => {
+			return session.mode === "kirjoituspeli"
+		}).length
+		let mixedGame = this.state.gameSessionsFiltered.filter(session => {
+			return session.mode === "sekapeli"
+		}).length
+		let multipleChoiceGame = this.state.gameSessionsFiltered.filter(session => {
+			return session.mode === "monivalintapeli"
+		}).length
+		this.setState({ writingGameCount: writingGame })
+		this.setState({ mixedGameCount: mixedGame })
+		this.setState({ multipleChoiceGameCount: multipleChoiceGame })
+	}
+
 	render() {
+		console.log(this.state.gameSessions)
 		if (this.state.redirect) {
 			return (
 				<Redirect to={{
@@ -35,8 +85,13 @@ class Statistics extends React.Component {
 				<Link to='/admin'>
 					<button className="gobackbutton">Takaisin</button>
 				</Link>
-				<p>Lällällää</p>
-			</div >
+				<h2>Pelin statistiikka</h2>
+				<p>Pelejä pelattu: {this.state.gameSessionsFiltered.length} kappaletta</p>
+				<p>Peliä pelattu yhteensä: {this.state.timePlayed} sekuntia</p>
+				<p>Kirjoituspelejä pelattu: {this.state.writingGameCount} kpl</p>
+				<p>Monivalintapelejä pelattu: {this.state.multipleChoiceGameCount} kpl</p>
+				<p>Sekapelejä pelattu: {this.state.mixedGameCount} kpl</p>
+			</div>
 		)
 	}
 }
