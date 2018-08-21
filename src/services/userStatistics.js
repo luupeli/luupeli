8cc58cb6921385821861577bfb0d271cc55c1458
Nabscina <bebenoctescere@gmail.com>
2018-08-22 00:22:08 +0300

    show information about user's best game on their profile page (unless user has played no games (at least that's how I hope it works))

--- src/services/userStatistics.js
+++ src/services/userStatistics.js
@@ -19,6 +19,17 @@ if (process.env.NODE_ENV === 'test') {
     top50Url = top50
 }
 
+
+const usersBestGames = '/api/gamesessions/top_list_game?limit=50'
+let bestGamesUrl = ''
+if (process.env.NODE_ENV === 'test') {
+    bestGamesUrl = 'http://luupeli-dev.herokuapp.com' + usersBestGames
+} else if (process.env.NODE_ENV === 'development') {
+    bestGamesUrl = 'http://localhost:3001' + usersBestGames
+} else {
+    bestGamesUrl = usersBestGames
+}
+
 const getTop50 = () => {
     const request = axios.get(top50Url)
     return request.then(response => { return response })
@@ -29,4 +40,9 @@ const getTotalGamesForIndividual = (userId) => {
     return request.then(response => { return response })
 }
 
-export default { getTop50, getTotalGamesForIndividual }
+const getUsersBestGames = (userId) => {
+    const request = axios.get(`${bestGamesUrl}&user=${userId}`)
+    return request.then(response => { return response })
+}
+
+export default { getTop50, getTotalGamesForIndividual, getUsersBestGames }
