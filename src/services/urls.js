0e2d2811231d277873a6dba31c9580576e810cff
lesktimo <l3skt1m@gmail.com>
2018-08-23 15:55:37 +0300

    fixed minor copypastebug

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,9 +1,7 @@
 const getUrl = () => {
   let baseUrl = ''
   if (process.env.NODE_ENV === 'development') {
-    baseUrl = 'http://localhost:3001' + url
-  } else {
-    baseUrl = url
+    baseUrl = 'http://localhost:3001'
   }
   return baseUrl
 }
